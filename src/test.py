030b09e6eee3a96892070b7412116366816e5eb9
Biaze7 <82425236+Biaze7@users.noreply.github.com>
2021-04-22 17:49:20 -0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -71,9 +71,9 @@ def test(cfg: Namespace) -> None:
         out = np.reshape(out, (768, 1280, 3))
         out = np.transpose(out, (2, 0, 1))
 
-        #y = T.cat((img[0], out), dim=2)
+        y = T.cat((img[0], out), dim=2)
         #y = T.cat((out), dim=0)
-        y = T.cat((img[], out), dim=2)
+        #y = T.cat((img[], out), dim=2)
         save_imgs(
             imgs=y.unsqueeze(0),
             to_size=(3, 768, 2 * 1280),
