6b8b1285b90b54e7993998f3eb22a9b15438b7f9
Biaze7 <82425236+Biaze7@users.noreply.github.com>
2021-04-28 12:35:23 -0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -93,9 +93,9 @@ def train(cfg: Namespace) -> None:
                     tb_writer.add_histogram(name, param, ts)
 
                 logger.debug(
-                    with T.Session() as sess:
-                        result = model.encoded.eval()
-                        print(result)
+                with T.Session() as sess:
+                    result = model.encoded.eval()
+                    print(result)
                     "[%3d/%3d][%5d/%5d] avg_loss: %.8f"
                     % (
                         epoch_idx,
